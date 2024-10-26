7a3a28ef3c8551ce3e003360da434abacc86047f
Arbaztalentxcel <talentxcelservices@gmail.com>
2024-10-26 19:53:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Career from "./pages/career";
+import Modal from "./Components/Modal";
+import Sidebar from "./Components/Sidebar";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/modal" element={<Modal />} />
+        <Route path='/sidebar' element={<Sidebar />}
+        
+        
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
